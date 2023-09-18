eae4f96155169e4ac3c48f44cc10e9d5b92c1adc
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2023-09-18 21:10:17 +0600

    add footer components

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div>
+      <p>All @ copyright reserve by shuvajit maitra</p>
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
 import "../../../src/App.css";
+import Footer from "../Footer/Footer";
 
 const Home = () => {
   return (
     <div>
       <Header></Header>
-      <h1>This is home components</h1>
+      <h3>This is home components</h3>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
